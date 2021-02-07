99e0d0f0141f306f128d7775c4e84f56de9e9e5c
aeryilmaz <aeryilmaz@outlook.com>
2021-02-07 23:06:11 +0300

    start project

---
+++ src/main/java/com/aeryilmaz/shoppingapp/AeryilmazShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.aeryilmaz.shoppingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeryilmazShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeryilmazShoppingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeryilmaz/shoppingapp/AeryilmazShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeryilmaz.shoppingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeryilmazShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
